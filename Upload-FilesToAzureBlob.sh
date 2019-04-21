bc700e19880bc490613cb1f2a5101cb9f949969e
AtomicGaryBusey <aharmon@zebeth.org>
2019-04-21 01:20:12 -0700

    Add files via upload

---
+++ Upload-FilesToAzureBlob.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Copy original logs to archival folder.
+copy /home/minecraft/*.gz /home/minecraft/logs/originals/
+
+# Copy working logs to working folder.
+copy /home/minecraft/*.gz /home/minecraft/logs/parsed/
+
+# Extract logs from GZip files.
+gunzip /home/minecraft/logs/parsed/*.gz
+
+# Remove IP addresses.
+sed -r 's/(\b[-1-9]{1,3}\.){3}[0-9]{1,3}\b'/REDACTED/ /home/minecraft/logs/parsed/*.log -i
+
+# Upload new logs to Azure Storage.
+azcopy --source /home/minecraft/logs/parsed/ --destination https://account.blob.core.windows.net/blobcontainer -dest-sas "<SASURI>" --recursive --quiet
