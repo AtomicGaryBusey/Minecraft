916ad61f2970f5dc1d1f03423e1175ce6ee2060e
AtomicGaryBusey <aharmon@zebeth.org>
2019-04-21 15:49:06 -0700

    Update RedactAndUploadFilesToAzureBlobStorage.sh

--- RedactAndUploadFilesToAzureBlobStorage.sh
+++ RedactAndUploadFilesToAzureBlobStorage.sh
@@ -2,6 +2,8 @@
 
 # This script was used to push Minecraft logs from a Linux host to Azure Blob Storage to be served on a website.
 # IP Addresses are redacted for privacy.
+# Requires AzCopy: https://aka.ms/AzCopyLinux
+
 
 # Copy original logs to archival folder.
 copy /home/minecraft/*.gz /home/minecraft/logs/originals/
